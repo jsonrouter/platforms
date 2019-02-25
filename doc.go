48904f54256e13720c540063165289fe1c018d7e
Joshua Medley <jmedleyx7@gmail.com>
2019-02-25 12:49:45 +0000

    testing first godoc example

--- doc.go
+++ doc.go
@@ -6,16 +6,18 @@ Appengine:
  import (
  	"github.com/jsonrouter/platforms/appengine"
  	ht "net/http"
+ 	"github.com/jsonrouter/core/openapi/v2"
+ 	"github.com/jsonrouter/core/validation"
  )
-
+ 
  func TestServer() {
  	spec := openapiv2.New("localhost", "TITLE")
  	
  	service, err = jsonrouter.New(spec)
  	if (err != nil){
-		// Handle Error //
+ 		// Handle Error //
  	}
-
+ 
  	panic(
  		ht.ListenAndServe(
  			fmt.Sprintf(
@@ -25,6 +27,13 @@ Appengine:
  			service,
  		),
  	)
+ }
+ 
+ func (app *App) api_product_get(req http.Request) *http.Status {
+ 	
+ 	// Do something 
+ 	
+ 	return nil
  }
 
 Fasthttp:
@@ -32,17 +41,38 @@ Fasthttp:
  import (
  	"github.com/jsonrouter/logging/testing"
  	"github.com/jsonrouter/platforms/fasthttp"
+ 	"github.com/jsonrouter/core/openapi/v2"
+ 	"github.com/jsonrouter/core/validation"
+ 
  )
-
+ 
  func TestServer() {
  	log := logs.NewClient().NewLogger()
  	spec := openapiv2.New("localhost", "TITLE")
  	
  	_, service = jsonrouter.New(log, spec)
-
+ 
+ 
+ 	api := service.Add("/api")
+ 
+ 	api.Add("/product").Param(validation.StringExact(30), "productID").GET(
+ 		app.api_product_get,
+ 	).Description(
+ 		"Gets the specified product",
+ 	).Response(
+ 		Product{},
+ 	)
+ 
  	panic(
  		service.Serve(PORT),
  	)
+ }
+ 
+ func (app *App) api_product_get(req http.Request) *http.Status {
+ 	
+ 	// Do something 
+ 	
+ 	return nil
  }
 
 Standard:
@@ -50,18 +80,35 @@ Standard:
  import (
  	"github.com/jsonrouter/logging/testing"
  	"github.com/jsonrouter/platforms/standard"
+ 	"github.com/jsonrouter/core/openapi/v2"
+ 	"github.com/jsonrouter/core/validation"
  	ht "net/http"
  )
-
+ 
+ type Product struct {
+ 	name string
+ }
+ 
  func TestServer() {
  	log := logs.NewClient().NewLogger()
  	spec := openapiv2.New("localhost", "TITLE")
  	
  	service, err = jsonrouter.New(log, spec)
  	if (err != nil){
-		// Handle Error //
+ 		// Handle error 
  	}
-
+ 
+ 
+ 	api := service.Add("/api")
+ 
+ 	api.Add("/product").Param(validation.StringExact(30), "productID").GET(
+ 		app.api_product_get,
+ 	).Description(
+ 		"Gets the specified product",
+ 	).Response(
+ 		Product{},
+ 	)
+ 
  	panic(
  		ht.ListenAndServe(
  			fmt.Sprintf(
@@ -71,6 +118,13 @@ Standard:
  			service,
  		),
  	)
+ }
+ 
+ func (app *App) api_product_get(req http.Request) *http.Status {
+ 	
+ 	// Do something 
+ 	
+ 	return nil
  }
 
 */
